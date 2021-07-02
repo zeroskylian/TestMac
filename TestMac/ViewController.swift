368d8d9d45e7c3680ebf16c2b09162bae18a655f
lianxinbo <lianxinbo@hengli.com>
2021-07-02 17:41:47 +0800

    Initial Commit

---
+++ TestMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestMac
+//
+//  Created by lian on 2021/7/2.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
