9785f396a3d6366d0be7f30dd5e4e193c1328234
lianxinbo <lianxinbo@hengli.com>
2022-01-04 19:12:32 +0800

    增加qrcode

--- TestMac/ViewController.swift
+++ TestMac/ViewController.swift
@@ -7,11 +7,20 @@
 
 import Cocoa
 import SwiftSoup
+import EFQRCode
 
 class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let imageview = NSImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.addSubview(imageview)
+        
+        if let image = EFQRCode.generate(for: "https://www.baidu.com") {
+            let nsImage = NSImage.init(cgImage: image, size: CGSize(width: 100, height: 100))
+            imageview.image = nsImage
+        }
+        
         
     }
     
